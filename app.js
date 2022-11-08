649625247ed2c76b65113f90f28486a50ce68d5b
civallerij <civallerijuan@gmail.com>
2022-11-07 21:30:20 -0300

    2da entrega proyecto final

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-let nombre = prompt ("ingrese su nombre")
-let apellido = prompt ("ingrese su apellido")
-let ciudad = prompt ("ingrese su cuidad")
-let domicilio = prompt ("ingrese su domicilio")
+
+function Producto (nombre, marca, precio) {
+        this.nombre = nombre
+        this.marca = marca
+        this.precio = precio
+}
+const producto1 = new Producto ("televisor","lg",56000)
+const producto2 = new Producto ("televisor","hitachi",62000)
+const producto3 = new Producto ("televisor","samsung",70000)
+const producto4 = new Producto ("celular","iphone",100000)
+const producto5 = new Producto ("celular","motorala",90000)
+const producto6 = new Producto ("celular","samsung",80000)
+const producto7 = new Producto ("computadora","lenovo",85000)
+const producto8 = new Producto ("computadora","samsung",76000)
+const producto9 = new Producto ("computadora","hp",70000)
+
+const productos = [producto1, producto2, producto3,producto4, producto5, producto6,producto7, producto8, producto9]
+
 let producto = prompt ("elija un producto televisor celular computadora")
 
-function productos () {
-    console.log (producto)
-}
-
-let marca = ("marca")
-
-function marcas () {
-    console.log (marca)
-}
-
 if (producto == "televisor" || producto == "celular" || producto == "computadora") {
-if (producto == "televisor"){
-    productos ()
-    let marca = prompt ("elija la marca de su televisor lg hitachi samsung")
-    if (marca == "lg" || marca == "hitachi" || marca == "samsung")
-    marcas()
-    alert ("felicitaciones " + nombre + " " + apellido + " ha adquirido un/a " + producto + " marca " + marca + " sera enviado a " + domicilio + " en la ciudad de " + ciudad)
-}
-if (producto == "celular"){
-    productos ()
-    let marca = prompt ("elija la marca de su de su celular iphone samsung motorola")
-    if (marca == "iphone" || marca == "samsung" || marca == "motorola")
-    marcas()
-    alert ("felicitaciones " + nombre + " " + apellido + " ha adquirido un/a " + producto + " marca " + marca + " sera enviado a " + domicilio + " en la ciudad de " + ciudad)
-}
-if (producto == "computadora"){
-    productos ()
-    let marca = prompt ("elija la marca de su computadora lenovo samsung hp")
-    if (marca == "lenovo" || marca == "samsung" || marca == "hp")
-    marcas()
-    alert ("felicitaciones " + nombre + " " + apellido + " ha adquirido un/a " + producto + " marca " + marca + " sera enviado a " + domicilio + " en la ciudad de " + ciudad)
-}
-}else {
-    alert (nombre + " " + apellido + " el producto que usted desea comprar no existe ")
-} 
+    if (producto == "televisor"){
+        const marcaProducto = prompt ("elija la marca de su televisor lg hitachi samsung")
+        const filter2 = productos.filter ((productos)=> productos.marca === marcaProducto)
+        console.log(filter2) }
+    if (producto == "celular"){
+        const marcaProducto = prompt ("elija la marca de su celular iphone motorola samsung")
+        const filter2 = productos.filter ((productos)=> productos.marca === marcaProducto)
+        console.log(filter2) }
+    if (producto == "computadora"){
+        const marcaProducto = prompt ("elija la marca de su computadora lenovo samsung hp")
+        const filter2 = productos.filter ((productos)=> productos.marca === marcaProducto)
+        console.log(filter2) }
+    }   else {
+        alert ("el producto que usted desea comprar no existe") }
+    
+
+
+
 
 
